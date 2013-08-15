37a1dcfc1788cf3ceb1fa4a4f35a2b390bcfb034
Liviu Cara <carliv@users.noreply.github.com>
2013-08-15 13:21:23 +0300

    mtk 6589 tweaks

--- nandroid.c
+++ nandroid.c
@@ -375,6 +375,9 @@ int nandroid_backup(const char* backup_path)
     if (0 != (ret = nandroid_backup_partition(backup_path, "/boot")))
         return ret;
 
+    if (0 != (ret = nandroid_backup_partition(backup_path, "/uboot")))
+        return ret;
+
     if (0 != (ret = nandroid_backup_partition(backup_path, "/recovery")))
         return ret;
 
@@ -748,8 +751,17 @@ int nandroid_restore(const char* backup_path, int restore_boot, int restore_syst
 
     int ret;
 
-    if (restore_boot && NULL != volume_for_path("/boot") && 0 != (ret = nandroid_restore_partition(backup_path, "/boot")))
-        return ret;
+    if (restore_boot)
+    {
+        if (NULL != volume_for_path("/boot") && 0 != (ret = nandroid_restore_partition(backup_path, "/boot")))
+            return ret;
+
+        if (NULL != volume_for_path("/uboot") && 0 != (ret = nandroid_restore_partition(backup_path, "/uboot")))
+            return ret;
+
+        if (NULL != volume_for_path("/recovery") && 0 != (ret = nandroid_restore_partition(backup_path, "/recovery")))
+            return ret;
+    }
 
     struct stat s;
     Volume *vol = volume_for_path("/wimax");
